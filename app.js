3452c20e13d5c2956fbc9851f5f706cfd6cf93fe
ash47 <ash.fourtyseven@gmail.com>
2015-03-29 10:00:08 +1100

    Added bare basic spammer

---
+++ app.js
@@ -0,0 +1,115 @@
+var http = require('http');
+
+// Voting info
+var hostName = 'southpark.cc.com';
+var postPath = '/feeds/blog/vote';
+var postData = {
+    action: 'polls',
+    view: 'process',
+    poll_id: '51',
+    poll_51: '102',
+    poll_51_nonce: 'ce0ecfe4de'
+};
+
+// Max number of threads to use to vote
+var maxThreads = 50;
+
+// Converts KV pairs into a URL encoded form data
+function formFormat(data) {
+    var k, v;
+
+    return ((function() {
+        var _results;
+
+        _results = [];
+        for (k in data) {
+            v = data[k];
+            _results.push("" + k + "=" + encodeURIComponent(v));
+        }
+
+        return _results;
+    })()).join('&');
+};
+
+var activeThreads = 0;
+function doPost(host, path, data, callback) {
+    var options = {
+        method: 'POST',
+        host: host,
+        port: 80,
+        path: path,
+        headers: {
+            'User-Agent': 'Southpark vote spammer'
+        },
+        agent:false
+    };
+
+    // Grab form data
+    var formData;
+    if (data) {
+        formData = formFormat(data);
+        options.headers['Content-Type'] = 'application/x-www-form-urlencoded';
+        options.headers['Content-Length'] = formData.length;
+        options.headers['Connection'] = 'Keep-Alive';
+    }
+
+    // Create the request
+    var req = http.request(options, callback);
+
+    // Handle disconnect error
+    req.on('error', function(error) {
+        console.log('HTTP error occurred: ' + error.message + ' (' + activeThreads + '/' + maxThreads + ')');
+        onVoteError();
+    });
+
+    // Submit form data
+    if (formData) {
+        req.write(formData);
+    }
+
+    return req.end();
+}
+
+function onVoteError() {
+    // This thread is no longer active
+    activeThreads--;
+
+    // Adjust max threads down
+    maxThreads = maxThreads/2;
+    if(maxThreads < 1) maxThreads = 1;
+
+    // Queue another vote
+    spamVotes();
+}
+
+var totalVotes = 0;
+function spamVotes() {
+    if(activeThreads >= maxThreads) return;
+
+    activeThreads++;
+    if(activeThreads < maxThreads) {
+        spamVotes();
+    }
+
+    doPost(hostName, postPath, postData, function(res) {
+        if(res.statusCode == 200) {
+            console.log('Vote added! (' + (++totalVotes) + ' votes so far) (' + activeThreads + '/' + maxThreads + ')');
+        } else {
+            console.log('Vote failed!');
+            onVoteError();
+            return;
+        }
+
+        // This thread is no longer active
+        activeThreads--;
+
+        // This was a success, increase max threads
+        maxThreads += 0.1;
+
+        // Add another vote
+        spamVotes();
+    });
+}
+
+// Begin spamming votes
+spamVotes();
